9f92d22377d371c3747a0ff48518453eced0e212
steven9587 <steven9587@gmail.com>
2018-11-29 23:02:50 +0800

    recyclerView

--- app/src/main/java/com/steven/atm/MainActivity.java
+++ app/src/main/java/com/steven/atm/MainActivity.java
@@ -1,12 +1,18 @@
 package com.steven.atm;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,15 +20,16 @@ import java.util.List;
 public class MainActivity extends BasaActivity {
     private static final int RC_LOGIN = 100;
     boolean login = false;
-    List<String> fruits = Arrays.asList("香蕉","鳳梨","芭樂");
+    List<String> fruits = Arrays.asList("香蕉", "鳳梨", "芭樂");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(!login){
-            Intent intent = new Intent(this,Login.class);
+        if (!login) {
+            Intent intent = new Intent(this, Login.class);
             //startActivity(intent);
-            startActivityForResult(intent,RC_LOGIN);
+            startActivityForResult(intent, RC_LOGIN);
         }
         //listView建立清單
         //listView();
@@ -31,11 +38,41 @@ public class MainActivity extends BasaActivity {
         recyclerView.setHasFixedSize(true);
         //new出linear(一列一列的)
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new FruitAdapter());
+    }
+
+    class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHolder> {
+        @NonNull
+        @Override
+        public FruitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            View view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1,parent,false);
+            FruitViewHolder fruitViewHolder = new FruitViewHolder(view);
+            return fruitViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull FruitViewHolder holder, int position) {
+            holder.fruitname.setText(fruits.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return fruits.size();
+        }
+
+        class FruitViewHolder extends RecyclerView.ViewHolder{
+            TextView fruitname;
+            public FruitViewHolder(View itemView) {
+                super(itemView);
+                fruitname = findViewById(android.R.id.text1);
+            }
+        }
     }
 
     private void listView() {
         ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fruits);
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(adapter);
     }
@@ -43,10 +80,10 @@ public class MainActivity extends BasaActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RC_LOGIN){
-            if(resultCode != RESULT_OK ){
+        if (requestCode == RC_LOGIN) {
+            if (resultCode != RESULT_OK) {
                 finish();
-            }else{
+            } else {
                 //TODO: if nickname / age /gender exist
 //                String saveNickname = getSharedPreferences("user",MODE_PRIVATE)
 //                        .getString("USERNICKNAME", null);
@@ -55,7 +92,7 @@ public class MainActivity extends BasaActivity {
 //                int saveGender = getSharedPreferences("user",MODE_PRIVATE)
 //                        .getInt("USERGENDER", 0);
                 if (!user.isValid()) {
-                    Intent nickname = new Intent(this,NicknameActivity.class);
+                    Intent nickname = new Intent(this, NicknameActivity.class);
                     startActivity(nickname);
                 }
             }
